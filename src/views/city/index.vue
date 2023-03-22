8282b06dd3085781c256c726efb067bc7cac114d
Yinkl7 <381964421@qq.com>
2023-03-22 21:36:43 +0800

    featrue request

--- src/views/city/index.vue
+++ src/views/city/index.vue
@@ -1,12 +1,37 @@
 <script setup>
-</script>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+import httpRequest from '@/service/request'
+
+const router = useRouter()
+// 搜索框
+const searchValue = ref("")
+const cancelClick = () => {
+  router.back()
+}
+
+// tab
+const active = ref(0)
+
+httpRequest.get({
+  url: '/city/all'
+}).then(res => {
+  console.log("data========= ", res)
+}).catch(err => {
+  console.log('error============ ', err)
+})
+
+</script> 
 
 <template>
   <div class="city">
-    city
-    <div v-for="item in 100">
-      hhh
-    </div>
+    <van-search v-model="searchValue" placeholder="" shape="round" show-action @cancel="cancelClick" />
+
+    <van-tabs v-model:active="active" color="#ff9854">
+      <van-tab title="国内.港澳台">内容 1</van-tab>
+      <van-tab title="海外">内容 2</van-tab>
+    </van-tabs>
   </div>
 </template>
 
